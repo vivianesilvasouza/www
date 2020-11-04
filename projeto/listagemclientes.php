d3a6b42da10ddff801a503bb402a7593befaaa90
viviane souza <vivianedasilvasouza2020@gmail.com>
2020-11-04 16:23:18 -0300

    continuação

--- projeto/listagemclientes.php
+++ projeto/listagemclientes.php
@@ -1,5 +1,5 @@
 <?php
-
+header("Content-Type:text/html; charset=latin1");
 $servidor = "localhost";
 $usuario = "root" ;
 $senha = "";
@@ -22,10 +22,65 @@ $comandoSQL = " select * from clientes;";
 // Para ver se está funcionando corretamente
 
 //echo $comandoSQL; mostra se deu certo 
-
+//4
 $registros = mysqli_query($con, $comandoSQL) or 
     die("Erro na seleção de clientes: <br>" . 
         mysqli_error($con) );
+
+//5 verificar o número de registros existentes
+$linhas = mysqli_num_rows($registros);
+
+// se não ter registros (linhas=0) , interromper o programa
+
+    if($linhas <1)
+    {
+        die("Cadastro de times está vazio! - Programa Interropido");
+    }
+    echo "Encontrado $linhas clientes <hr>";
+
+// 6 repetir o numero de linhas que tem 
+// mostrar os dados de cada linha 
+    $contador=0;
+         while ($contador <$linhas)
+         {
+
+            // Criando a matriz $dados
+            $dados = mysqli_fetch_array($registros);
+
+            echo "id: " . $dados["id"] . "<br>";
+            echo "nome: " . $dados["nome"]  . "<br>";
+            echo "dataNascimento: " . $dados["dataNascimento"]  . "<br>";
+            echo "cpf: " . $dados["cpf"]  . "<br>";
+            echo "rg: " . $dados["rg"]  . "<br>";
+            echo "sexo: " . $dados["sexo"]  . "<br>";
+            echo "email: " . $dados["email"]  . "<br>";
+            echo "telefone: " . $dados["telefone"]  . "<br>";
+            echo "celular: " . $dados["celular"]  . "<br>";
+            echo "pais: " . $dados["pais"]  . "<br>";
+            echo "estado: " . $dados["estado"]  . "<br>";
+            echo "cidade: " . $dados["cidade"]  . "<br>";
+            echo "bairro: " . $dados["bairro"]  . "<br>";
+            echo "cep: " . $dados["cep"]  . "<br>";
+            echo "rua: " . $dados["rua"]  . "<br>";
+            echo "numero: " . $dados["numero"]  . "<br>";
+            echo "complemento: " . $dados["complemento"]  . "<br>";
+            echo "referencia: " . $dados["referencia"]  . "<hr>";
+
+
+
+
+
+
+
+
+             $contador++;
+             
+             //echo"$contador <br>";
+         }
+
+
+
+
 
 
 
