468a9ab01df7252c0c2a874894786a29e7f85156
viviane souza <vivianedasilvasouza2020@gmail.com>
2020-11-04 13:10:45 -0300

    registros de cliente está ok

--- projeto/listagemclientes.php
+++ projeto/listagemclientes.php
@@ -13,4 +13,20 @@ $banco = "dinocoforcar";
 mysqli_select_db($con, "$banco") or 
     die("Erro na seleção de bancos de dados" . mysqli_error($con)) ;
 
+//3 colocar comando de seleção de dados 
+
+$comandoSQL = " select * from clientes;";
+
+// imprimir a variavel na tela 
+// copiar o que aparece no conmsole do MYSQL
+// Para ver se está funcionando corretamente
+
+//echo $comandoSQL; mostra se deu certo 
+
+$registros = mysqli_query($con, $comandoSQL) or 
+    die("Erro na seleção de clientes: <br>" . 
+        mysqli_error($con) );
+
+
+
 ?>
